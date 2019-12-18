1731c649c59fb64c5218149696af59b5f8ad8670
vbraz <victorfbraz@gmail.com>
2019-12-18 12:02:22 -0300

    tapa

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ function list_cards(){
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var myObj = JSON.parse(this.responseText);
+			var card = "<div class='card-columns'>";
 			for(var i = 0; i < myObj.length; i++) {
 				console.log(myObj[i].name);
-				document.getElementById("card").innerHTML += "<div class='card col-3-md'>";
-				document.getElementById("card").innerHTML += "<img src='"+myObj[i].pic+"' class='card-img-top' alt='"+myObj[i].name+"'>";
-				document.getElementById("card").innerHTML += "<div class='card-body p-0 pt-1'>";
-				document.getElementById("card").innerHTML += "<h3>"+myObj[i].name+"</h3>";
-				document.getElementById("card").innerHTML += "</div>";
-				document.getElementById("card").innerHTML += "</div>";
+				card += "<div class='card col-3-md'>";
+				card += "<h3>"+myObj[i].name+"</h3>";
+				card += "<img src='"+myObj[i].pic+"' class='card-img-top' alt='"+myObj[i].name+"'>";
+				card += "<div class='card-body p-0 pt-1'>";
+				card += myObj[i].text;
+				card += "</div>";
+				card += "</div>";
 			}
+			card += "</div>";
+			document.getElementById("card").innerHTML += card;
 		}
 	};
-	xmlhttp.open("GET", "https://raw.githubusercontent.com/vbraz/make-drinks/master/drinks.json?token=ABZUC74MB4ZPJHMVGGICTC257I6PW", true);
+	xmlhttp.open("GET", "https://raw.githubusercontent.com/vbraz/make-drinks/master/drinks.json?token=ABZUC72EMOTW6DRAZBFMVP257I6Z4", true);
 	xmlhttp.send();
 }
