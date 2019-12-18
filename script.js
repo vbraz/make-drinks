44c31570dad8930846c403ccc74edf3afc757177
vbraz <victorfbraz@gmail.com>
2019-12-18 11:52:55 -0300

    feito!

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function list_cards(){
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var myObj = JSON.parse(this.responseText);
-			console.log(myObj.name);
+			for(var i = 0; i < myObj.length; i++) {
+				console.log(myObj[i].name);
+				document.getElementById("card").innerHTML += "<div class='card col-3-md'>";
+				document.getElementById("card").innerHTML += "<img src='"+myObj[i].pic+"' class='card-img-top' alt='"+myObj[i].name+"'>";
+				document.getElementById("card").innerHTML += "<div class='card-body p-0 pt-1'>";
+				document.getElementById("card").innerHTML += "<h3>"+myObj[i].name+"</h3>";
+				document.getElementById("card").innerHTML += "</div>";
+				document.getElementById("card").innerHTML += "</div>";
+			}
 		}
 	};
-	xmlhttp.open("GET", "drinks.json", true);
+	xmlhttp.open("GET", "https://raw.githubusercontent.com/vbraz/make-drinks/master/drinks.json?token=ABZUC74MB4ZPJHMVGGICTC257I6PW", true);
 	xmlhttp.send();
 }
