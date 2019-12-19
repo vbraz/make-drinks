f806a73c6533af42a9cc42520567fc30e9425e84
Victor Braz <victorbraz@MacBook-Pro-de-Victor.local>
2019-12-18 23:00:59 -0300

    Pronto para o lançamento

--- public/script.js
+++ public/script.js
@@ -1,76 +1,70 @@
+
 function start(){
 	document.getElementById("detail").innerHTML = "";
 	document.getElementById("all").innerHTML = "";
 	var hash = window.location.hash;
-	if(hash){
-		detail(hash);
-	} else {
-		all();
-	}
+	var xmlhttp = new XMLHttpRequest();
+
+	//xmlhttp.open("GET", "drinks.json", true);
+	xmlhttp.open("GET", "https://raw.githubusercontent.com/vbraz/make-drinks/master/public/drinks.json", true);
+	xmlhttp.send();
+
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+				if(hash){ detail(hash, myObj);
+				} else  { all(myObj); }
+			}
+	};
 }
 
-function all(){
+function all(myObj){
 	document.getElementById("title").innerHTML = "Make Drinks";
-	document.getElementById("subtitle").innerHTML = "Quer aprender como fazer drinks e coquetéis como um verdadeiro barman? Explore o saboroso mundo do TheBar.com e descubra as melhores receitas de...";
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var myObj = JSON.parse(this.responseText);
-			var card = "<div class='card-columns'>";
-			for(var i = 0; i < myObj.length; i++) {
-				//console.log(myObj[i].name);
-				card += "<div class='card col-3-md mb-4' onclick=\"window.location.hash='"+myObj[i].id+"'; start();\">";
-				card += "<h3>"+myObj[i].name+"</h3>";
-				card += "<img src='"+myObj[i].pic+"' class='card-img-top shadow' alt='"+myObj[i].name+"'>";
-				card += "<div class='card-body p-0 pt-1'>";
-				card += myObj[i].text;
-				card += "</div>";
-				card += "</div>";
-			}
-			card += "</div>";
-			document.getElementById("all").innerHTML += card;
-		}
-	};
-	xmlhttp.open("GET", "drinks.json", true);
-	//xmlhttp.open("GET", "https://raw.githubusercontent.com/vbraz/make-drinks/master/public/drinks.json", true);
-	xmlhttp.send();
+	document.getElementById("subtitle").innerHTML = "Quer aprender como fazer drinks e coquetéis como um verdadeiro barman? Explore o saboroso mundo do Make Drinks e descubra as melhores receitas.";
+
+	var card = "<div class='card-columns'>";
+	for(var i = 0; i < myObj.length; i++) {
+		//console.log(myObj[i].name);
+		card += "<div class='card col-3-md mb-4' onclick=\"window.location.hash='"+myObj[i].id+"'; start();\">";
+		card += "<h3>"+myObj[i].name+"</h3>";
+		card += "<img src='"+myObj[i].pic+"' class='card-img-top shadow' alt='"+myObj[i].name+"'>";
+		card += "<div class='card-body p-0 pt-1'>";
+		card += myObj[i].text;
+		card += "</div>";
+		card += "</div>";
+	}
+	card += "</div>";
+	document.getElementById("all").innerHTML += card;
 }
 
-function detail(hash){
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var myObj = JSON.parse(this.responseText);
-			var detail = "", ingredient = "", make = "", count = 1;
-			for(var i = 0; i < myObj.length; i++) {
-				if(hash == "#"+myObj[i].id){
-					for(var ii = 0; ii < myObj[i].ingredients.length; ii++) {
-						ingredient += " - "+myObj[i].ingredients[ii].ingredient+"<br>";
-					}
-					for(var ii = 0; ii < myObj[i].make.length; ii++) {
-						make += "<p><b>"+count+".</b> "+myObj[i].make[ii].step+"</p>";
-						count = count + 1;
-					}
-					break;
-				}
+function detail(hash, myObj){
+	var detail = "", ingredient = "", make = "", count = 1;
+	for(var i = 0; i < myObj.length; i++) {
+		if(hash == "#"+myObj[i].id){
+			for(var ii = 0; ii < myObj[i].ingredients.length; ii++) {
+				ingredient += " - "+myObj[i].ingredients[ii].ingredient+"<br>";
 			}
-			detail += "<center><img src='"+myObj[i].pic+"' class='img-fluid mb-4 shadow' alt='"+myObj[i].name+"'></center>";
-			detail += "<h3>Modo de preparo</h3>"
-			detail += ingredient;
-			detail += "<hr class='mt-3 mb-3'>";
-			detail += "<h3>Ingredientes</h3>"
-			detail += make;
-			detail += "<center>";
-			detail += "<button type='button' class='btn btn-wb mt-3 mb-3 rounded-pill' onclick=\"window.location.hash=''; start();\">voltar</button> ";
-			detail += "<a href='https://wa.me/?text=https://make-drinks.firebaseapp.com/"+hash+"' role='button' class='btn  btn-success mt-3 mb-3 rounded-pill'>whatsapp</a>";
-			detail += "</center>";
-
-			document.getElementById("title").innerHTML = myObj[i].name;
-			document.getElementById("subtitle").innerHTML = myObj[i].text;
-			document.getElementById("detail").innerHTML += detail;
+			for(var ii = 0; ii < myObj[i].make.length; ii++) {
+				make += "<p><b>"+count+".</b> "+myObj[i].make[ii].step+"</p>";
+				count = count + 1;
+			}
+			break;
 		}
-	};
-	xmlhttp.open("GET", "drinks.json", true);
-	//xmlhttp.open("GET", "https://raw.githubusercontent.com/vbraz/make-drinks/master/public/drinks.json", true);
-	xmlhttp.send();
+	}
+	hash = hash.replace("#","%23");
+	detail += "<center><img src='"+myObj[i].pic+"' class='img-fluid mb-4 shadow' alt='"+myObj[i].name+"'></center>";
+	detail += "<h3>Ingredientes</h3>"
+	detail += ingredient;
+	detail += "<hr class='mt-3 mb-3'>";
+	detail += "<h3>Modo de preparo</h3>"
+	detail += make;
+	detail += "<center>";
+	detail += "<button type='button' class='btn btn-wb mt-3 mb-3 rounded-pill' onclick=\"window.location.hash=''; start();\">voltar</button> ";
+	detail += "<a href='https://wa.me/?text="+myObj[i].text+" \n https://make-drinks.web.app/"+hash+"' data-action='share/whatsapp/share' role='button' class='btn  btn-success mt-3 mb-3 rounded-pill'>whatsapp</a>";
+	detail += "</center>";
+
+	document.getElementById("title").innerHTML = myObj[i].name;
+	document.getElementById("subtitle").innerHTML = myObj[i].text;
+	document.getElementById("detail").innerHTML += detail;
 }
+
